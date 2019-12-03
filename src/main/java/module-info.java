26497d1125a4a3e7079f5e7d6c3e28b2231a0012
ron <ron@ronella.xyz>
2019-12-03 22:17:00 +1300

    Update dependencies.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,9 @@
 module template.java.lib {
+    requires org.slf4j;
     requires org.apache.logging.log4j;
     requires org.apache.logging.log4j.core;
     requires java.scripting;
+
+    requires xyz.ronella.logging.logger.plus;
+    requires xyz.ronella.casual.trivial;
 }
--- src/main/java/xyz/ronella/template/lib/Main.java
+++ src/main/java/xyz/ronella/template/lib/Main.java
@@ -1,11 +1,11 @@
 package xyz.ronella.template.lib;
 
-import org.apache.logging.log4j.LogManager;
-import xyz.ronella.template.lib.commons.LoggerPlus;
+import org.slf4j.LoggerFactory;
+import xyz.ronella.logging.LoggerPlus;
 
 public class Main {
 
-    private final static LoggerPlus LOGGER_PLUS = new LoggerPlus(LogManager.getLogger(Main.class));
+    private final static LoggerPlus LOGGER_PLUS = new LoggerPlus(LoggerFactory.getLogger(Main.class));
 
     public String hello(String name) {
         try (var mLOG = LOGGER_PLUS.logByMethodCall("hello")) {
