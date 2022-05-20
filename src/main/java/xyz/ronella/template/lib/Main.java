b22d5e7cadd6675a51c1d1e0e9943135bb4254e3
Ron Webb <ron@ronella.xyz>
2022-05-20 15:19:24 +1200

    Code clean up.

--- src/main/java/xyz/ronella/template/lib/Main.java
+++ src/main/java/xyz/ronella/template/lib/Main.java
@@ -2,16 +2,21 @@ package xyz.ronella.template.lib;
 
 import org.slf4j.LoggerFactory;
 import xyz.ronella.logging.LoggerPlus;
+import xyz.ronella.trivial.decorator.Mutable;
+
+import java.util.function.Consumer;
 
 public class Main {
 
     private final static LoggerPlus LOGGER_PLUS = new LoggerPlus(LoggerFactory.getLogger(Main.class));
 
     public String hello(String name) {
-        try (var mLOG = LOGGER_PLUS.logByMethodCall("hello")) {
-            var output = String.format("Hello %s", name);
-            mLOG.debug(output);
-            return output;
+        try (var mLOG = LOGGER_PLUS.groupLog("hello")) {
+            final var mString = new Mutable<>("Hello ");
+            final Consumer<String> process = (___string) -> mString.set(mString.get().concat(___string));
+            process.accept("World");
+            mLOG.debug(mString.get());
+            return mString.get();
         }
     }
 
--- src/main/java/xyz/ronella/template/lib/commons/PackageMgr.java
+++
@@ -1,9 +0,0 @@
-package xyz.ronella.template.lib.commons;
-
-import java.nio.file.Paths;
-
-public class PackageMgr {
-    public static boolean isPackaged() {
-        return Paths.get("___package___").toFile().exists();
-    }
-}
